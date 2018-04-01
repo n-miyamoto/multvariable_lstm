73e8b1d385509bb7bb5e98c287a24cf01fcd76a9
n-miyamoto <scotsdowneroad@gmail.com>
2018-04-01 11:02:08 +0900

    implement stacked lstm

---
+++ stacked_lstm.py
@@ -0,0 +1,33 @@
+#!/USR/bin/env python
+# -*- coding: utf-8 -*- 
+import chainer
+import chainer.links as L
+import chainer.functions as F 
+
+class LSTM(chainer.Chain):
+    def __init__(self, in_units=1, hidden_units_l1=2, hidden_units_l2=2 , out_units=1, train=True):
+        super(LSTM, self).__init__(
+                l1=L.Linear(in_units, hidden_units_l1),
+                l2=L.LSTM(hidden_units_l1, hidden_units_l1),
+                l3=L.LSTM(hidden_units_l2, hidden_units_l2),
+                l4=L.Linear(hidden_units_l2, out_units),
+        )
+        self.train = True
+
+    def __call__(self, x, t): 
+        h = self.l1(x)
+        h = self.l2(h)
+        h = self.l3(h)
+        y = self.l4(h)
+        self.loss = F.mean_squared_error(y, t)
+        if self.train:
+            return self.loss
+        else:
+            self.prediction = y 
+        return self.prediction
+
+    def reset_state(self):
+        self.l2.reset_state()
+        self.l3.reset_state()
+
+
